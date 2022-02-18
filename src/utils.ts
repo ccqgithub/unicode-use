2a13cbbae804c5014705fdd1f9fdad6b1fcb8fe4
season chen <season.chen.i@foxmail.com>
2022-02-18 17:39:04 +0800

    init

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,9 @@ export const codePointToUTF16 = (point: number) => {
   if (point <= 0xffff) {
     return `\\u${point.toString(16)}`;
   }
-  const h = Math.floor((Number(`0x${point}`) - 0x10000) / 0x400) + 0xd800;
-  const l = ((0x1d306 - 0x10000) % 0x400) + 0xdc00;
+  const index = Number(`0x${point}`) - 0x10000;
+  const h = Math.floor(index / 0x400) + 0xd800;
+  const l = (index % 0x400) + 0xdc00;
   return `\\u${h.toString(16)}\\u${l.toString(16)}`;
 };
 
